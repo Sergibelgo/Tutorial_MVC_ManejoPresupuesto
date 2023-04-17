8de9d07734bf5c958a9206bb053de4e90554b4b4
Sergibelgo <sergibelgo1@gmail.com>
2023-04-17 09:05:27 +0200

    Finalizacion step 110

---
+++ Services/ITiposCuentasServices.cs
@@ -0,0 +1,9 @@
+﻿using Tutorial2ManejoPresupuesto.Models;
+
+namespace Tutorial2ManejoPresupuesto.Services
+{
+    public interface ITiposCuentasServices
+    {
+        public void Crear(TipoCuenta tipoCuenta);
+    }
+}
