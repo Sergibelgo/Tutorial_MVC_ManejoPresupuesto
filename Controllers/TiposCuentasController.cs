8433e08d5bd66efaf493b3bb59e0f835cdb602a6
Sergibelgo <sergibelgo1@gmail.com>
2023-04-17 08:51:05 +0200

    Step 109 finalizado

--- Controllers/TiposCuentasController.cs
+++ Controllers/TiposCuentasController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Tutorial2ManejoPresupuesto.Models;
 
 namespace Tutorial2ManejoPresupuesto.Controllers
 {
-    public class TiposCuentasController:Controller
+    public class TiposCuentasController : Controller
     {
+        private readonly string _configuration;
+        public TiposCuentasController(IConfiguration IConfiguration)
+        {
+            _configuration = IConfiguration.GetConnectionString("DefaultConnection");
+        }
         [HttpGet]
         public IActionResult Crear()
         {
+            using (var connection = new SqlConnection(_configuration))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            }
             return View();
         }
         [HttpPost]
