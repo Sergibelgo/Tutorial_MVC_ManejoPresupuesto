78717327b1a8f6e85e19343e9cd2db45427c5f31
Sergibelgo <sergibelgo1@gmail.com>
2023-04-19 09:39:53 +0200

    Step 133 finalizado

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial2ManejoPresupuesto.Models;
+using Tutorial2ManejoPresupuesto.Services;
+
+namespace Tutorial2ManejoPresupuesto.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriasService _categoriasService;
+        private readonly IUsuariosService _usuariosService;
+
+        public CategoriasController(ICategoriasService categoriasService, IUsuariosService usuariosService)
+        {
+            this._categoriasService = categoriasService;
+            this._usuariosService = usuariosService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Crear(Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoria);
+            }
+            var usuarioId = _usuariosService.GetUsuario();
+            categoria.UserId = usuarioId;
+            await _categoriasService.Create(categoria);
+            return RedirectToAction("Inedx");
+        }
+    }
+}
