9969d2936f4df1ad4821cbd09823d484c5e82dd8
Sergibelgo <sergibelgo1@gmail.com>
2023-04-19 14:07:44 +0200

    Step 138 finalizado

---
+++ Models/Transaccion.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial2ManejoPresupuesto.Models
+{
+    public class Transaccion
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaTransaccion { get; set; } = DateTime.Now;
+        public decimal Monto { get; set; }
+        public int TipoOperacionId { get; set; }
+        [StringLength(maximumLength:1000,ErrorMessage ="La nota no puede pasar de {1} caracteres")]
+        public string Nota { get; set; }
+        [Range(1,maximum:int.MaxValue, ErrorMessage = "Debe seleccionar una categoría")]
+        public int CuentaId { get; set; }
+        [Range(1,maximum:int.MaxValue,ErrorMessage ="Debe seleccionar una categoría")]
+        public int CategoriaId { get; set; }
+    }
+}
---
+++ Services/TransaccionesService.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Tutorial2ManejoPresupuesto.Models;
+
+namespace Tutorial2ManejoPresupuesto.Services
+{
+    public interface ITransaccionesService
+    {
+
+    }
+    public class TransaccionesService : ITransaccionesService
+    {
+        private readonly string connectionString;
+        public TransaccionesService(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task Crear(Transaccion transaccion)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"Transacciones_Insertar", new
+            {
+                transaccion.UsuarioId,
+                transaccion.FechaTransaccion,
+                transaccion.Monto,
+                transaccion.CategoriaId,
+                transaccion.CuentaId,
+                transaccion.Nota
+            });
+        }
+    }
+}
