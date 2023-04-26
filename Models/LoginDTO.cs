e4d15c804ca64e6efa2c52ad5d055794b21584eb
Sergibelgo <sergibelgo1@gmail.com>
2023-04-26 11:11:39 +0200

    Step 177 finalizado

---
+++ Models/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial2ManejoPresupuesto.Models
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.EmailAddress,ErrorMessage ="El campo {0} ha de ser de tipo {1}")]
+        public string Email { get; set; }
+        [Required (ErrorMessage ="El campo {0} es requerido")]
+        [DataType(DataType.Password, ErrorMessage = "El campo {0} ha de ser de tipo {1}")]
+        public string Password { get; set; }
+        public bool Recuerdame { get; set; }
+    }
+}
